c8ca5c958002a92d3168c7f27cdb288413fe42cf
William Shue <37516784+william-shue@users.noreply.github.com>
2020-03-17 20:30:35 -0400

    adding landing page and golang server

---
+++ diaverse/main.go
@@ -0,0 +1,35 @@
+package main
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type Welcome struct {
+	Name string
+	Time string
+}
+
+func main() {
+
+	welcome := Welcome{"Diaverse", time.Now().Format(time.Stamp)}
+
+	templates := template.Must(template.ParseFiles("templates/homepage.html"))
+
+	http.Handle("/webpage/",
+		http.StripPrefix("/webpage/",
+			http.FileServer(http.Dir("webpage"))))
+
+	http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
+
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name;
+		}
+		if err := templates.ExecuteTemplate(w, "homepage.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println(http.ListenAndServe(":8080", nil));
+}
