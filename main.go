e3704cde0d05d26904b89c9bcf283b18b7623449
William Shue <37516784+william-shue@users.noreply.github.com>
2020-03-21 17:37:45 -0400

    added endpoints and first handler

--- main.go
+++ main.go
@@ -8,10 +8,37 @@ import (
 func main() {
 
 	mux := http.DefaultServeMux
-
 	mux.Handle("/", http.FileServer(http.Dir("./Frontend/Login")))
+	mux.HandleFunc("/RegisterUser", handleLogin)
+	mux.HandleFunc("/LoginUser", )
+	mux.HandleFunc("/RegisterHardware", )
+	mux.HandleFunc("/CheckForExistingHardwareToken", )
+	mux.HandleFunc("/AuthenticateHardware", )
+	mux.HandleFunc("/GetScritpsByHardwareToken", )
+	mux.HandleFunc("/Logout", )
+	mux.HandleFunc("/GetUserInfoByUsername", )
+	mux.HandleFunc("/GetScriptResultsByHardwareToken", )
+	mux.HandleFunc("/UpdateUserInfo", )
+	mux.HandleFunc("/DeleteAccount", )
+	mux.HandleFunc("/DeleteScript", )
+	mux.HandleFunc("/UploadScript", )
 
 	print("running")
 
 	fmt.Println(http.ListenAndServe(":8080", mux))
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	uname := r.FormValue("username")
+	pass := r.FormValue("pass")
+
+	//verify in db
+	result := true
+
+	if result {
+		w.WriteHeader(http.StatusAccepted)
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
