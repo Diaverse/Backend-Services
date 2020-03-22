6e77ec116b577931dea4e7cb6ab60037aa6c1aa0
William Shue <37516784+william-shue@users.noreply.github.com>
2020-03-22 13:10:46 -0400

    db handlers

--- backend-services/db-handlers/main.go
+++ backend-services/db-handlers/main.go
@@ -1,29 +1,36 @@
 package main
 
-import "database/sql"
-import "fmt"
-import _ "github.com/go-sql-driver/mysql"
+import (
+        "database/sql"
+        "fmt"
+        _ "github.com/go-sql-driver/mysql"
+)
+
+type UserData struct {
+    Username, Password, Email, Hardwaretoken, FirstName, LastName, UserBio string
+}
+
+var db *sql.DB
+var err error
 
 func main() {
-        print("running")
+        println("running")
 
         //open the database connection
-        db, err := sql.Open("mysql", "USERNAME-GOES-HERE:PASSWORD-GOES-HERE@tcp(ENDPOINT:PORT-NUMBER)/DATABSE-NAME")
+        db, err := sql.Open("mysql", "")
         if err != nil {
-            panic(err)
+            fmt.Println(err.Error())
         }
 
-        //select a single row
-        //id := "username"
-        var col string
-        sqlStatment := `SELECT * FROM users`
-        row  := db.QueryRow(sqlStatment)
-        err  = row.Scan(&col)
+        var userDataStruct UserData
+        usersName := "william"
+        err = db.QueryRow("SELECT username, password, email, hardwaretoken, first_name, last_name, user_bio FROM users WHERE username = ?", usersName).Scan(&userDataStruct.Username, &userDataStruct.Password, &userDataStruct.Email, &userDataStruct.Hardwaretoken, &userDataStruct.FirstName, &userDataStruct.LastName, &userDataStruct.UserBio)
         if err != nil {
-            if err == sql.ErrNoRows {
-                fmt.Println("Zero rows found")
-            } else {
-                panic(err)
+            fmt.Println(err.Error())
         }
-    }
+
+        fmt.Printf("Username: %s\n Password: %s\n Email: %s\n HardwareToken: %s\n FirstName: %s\n LastName: %s\n UserBio: %s\n", userDataStruct.Username, userDataStruct.Password, userDataStruct.Email, userDataStruct.Hardwaretoken, userDataStruct.FirstName, userDataStruct.LastName, userDataStruct.UserBio)
+
+
+        //return err
 }
